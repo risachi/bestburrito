5b5202bf1e57cb107b2c6b77381a14b2aef2f6de
risachi <risachi@gmail.com>
2016-10-06 16:02:41 -0700

    build home and rate views

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment this line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '9.0'
 
 target 'best-burrito' do
   # Comment this line if you're not using Swift and don't want to use dynamic frameworks
@@ -7,8 +7,7 @@ target 'best-burrito' do
 
   # Pods for best-burrito
 
-  pod 'Firebase/Core'
-  pod 'Firebase/Database'
+  pod 'Firebase'
 
   target 'best-burritoTests' do
     inherit! :search_paths
--- best-burrito/RateViewController.swift
+++ best-burrito/RateViewController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  RateViewController.swift
 //  best-burrito
 //
 //  Created by Lisa on 10/5/16.
 //  Copyright © 2016 Lisa Hackenberger. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class RateViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
