d46a7fe73e606d5b4a6efd64f02f297723e89624
risachi <risachi@gmail.com>
2016-12-15 17:58:56 -0800

    first try map config

--- best-burrito/DetailViewController.swift
+++ best-burrito/DetailViewController.swift
@@ -10,8 +10,9 @@ import Foundation
 import UIKit
 import Firebase
 import GoogleMapsCore
+import GooglePlaces
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet var burritoNameLabel: UILabel!
     @IBOutlet weak var restaurantNameLabel: UILabel!
@@ -29,11 +30,98 @@ class DetailViewController: UIViewController {
     
     
     // Map
-    @IBOutlet weak var mapView: GMSMapView!
+    //@IBOutlet weak var mapView: GMSMapView!
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var placesClient: GMSPlacesClient!
+    var zoomLevel: Float = 15.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+        placesClient = GMSPlacesClient.shared()
     }
     
-
+    let camera = GMSCameraPosition.camera(withLatitude: defaultLocation.coordinate.latitude,
+                                          longitude: defaultLocation.coordinate.longitude,
+                                          zoom: zoomLevel)
+    mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
+    mapView.isMyLocationEnabled = true
+    mapView.settings.myLocationButton = true
+    mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    
+    // Add the map to the view, hide it until we've got a location update.
+    view.addSubview(mapView)
+    mapView.isHidden = true
+    
+    func updateMarkers() {
+        mapView.clear()
+        placesClient.currentPlace(callback: { (placeLikelihoods, error) -> Void in
+            if let error = error {
+                print("Current Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            if let likelihoodList = placeLikelihoods {
+                for likelihood in likelihoodList.likelihoods {
+                    let place = likelihood.place
+                    
+                    let marker = GMSMarker(position: place.coordinate)
+                    marker.title = place.name
+                    marker.snippet = place.formattedAddress
+                    marker.map = self.mapView
+                }
+            }
+        })
+    }
+    
+    // Handle incoming location events.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        print("Location: \(location)")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude,
+                                              zoom: zoomLevel)
+        
+        if mapView.isHidden {
+            mapView.isHidden = false
+            mapView.camera = camera
+        } else {
+            mapView.animate(to: camera)
+        }
+        
+        updateMarkers()
+    }
+    
+    // Handle authorization for the location manager.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+            // Display the map using the default location.
+            mapView.isHidden = false
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK.")
+        }
+    }
+    
+    // Handle location manager errors.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        print("Error: \(error)")
+    }
+    
 }
